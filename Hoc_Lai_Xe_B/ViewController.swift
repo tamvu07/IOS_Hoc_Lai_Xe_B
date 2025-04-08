d8b8fdd8891e710c87184873c6161a5f9f1d4bca
Tam Vu <tam_vu@onskyhealth.com>
2025-04-08 13:47:26 +0700

    *Update code

--- Hoc_Lai_Xe_B/ViewController.swift
+++ Hoc_Lai_Xe_B/ViewController.swift
@@ -7,63 +7,97 @@
 
 import UIKit
 import WebKit
+import Combine
 
 class ViewController: UIViewController, WKNavigationDelegate {
-    var webView: WKWebView!
+    
+    @IBOutlet weak var webView1: WKWebView!
+    @IBOutlet weak var selectBT: UIButton!
+    //    var webView: WKWebView!
+    
     var timer: Timer?
-
+    var clickIndicator: UIView!
+    
+    
+    @Published var isSelect: Bool = false
+    
+    var subscriptions = [AnyCancellable]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Initialize WKWebView
-        webView = WKWebView(frame: self.view.frame)
-        webView.navigationDelegate = self
-        self.view.addSubview(webView)
+        //        webView = WKWebView(frame: self.view.frame)
+        webView1.navigationDelegate = self
+        //        self.view.addSubview(webView)
+        
+        createClickIndicator()
         
         // Load the desired URL
         if let url = URL(string: "https://tuanlong.huelms.com/user/login/?logout=1") {
             let request = URLRequest(url: url)
-            webView.load(request)
+            webView1.load(request)
         }
+        
+        self.$isSelect
+            .sink(receiveValue: { [weak self] result in
+                if result {
+                    self?.startAutoClicking()
+                } else {
+                    self?.stopAutoClicking()
+                }
+            })
+            .store(in: &subscriptions)
     }
+    
+    @IBAction func btAction(_ sender: Any) {
+        isSelect.toggle()
+    }
+    
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         // Start the timer to click the element every 2 seconds
-        startAutoClicking()
+        //        startAutoClicking()
+    }
+    
+    func createClickIndicator() {
+        clickIndicator = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        clickIndicator.backgroundColor = UIColor.red
+        clickIndicator.layer.cornerRadius = 10
+        self.view.addSubview(clickIndicator)
     }
     
     func startAutoClicking() {
-        timer = Timer.scheduledTimer(timeInterval: 20.0, target: self, selector: #selector(autoClick), userInfo: nil, repeats: true)
+        selectBT.backgroundColor = UIColor.green
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(autoClick), userInfo: nil, repeats: true)
     }
     
     @objc func autoClick() {
+        var x = 340
+        var y = 640
         
-        // Thay đổi tọa độ x và y theo nhu cầu của bạn
-                let x = 10 // Tọa độ x
-                let y = 10 // Tọa độ y
-                let js = "var evt = new MouseEvent('click', {clientX: \(x), clientY: \(y), bubbles: true}); document.elementFromPoint(\(x), \(y)).dispatchEvent(evt);"
-                
-                webView.evaluateJavaScript(js) { (result, error) in
-                    if let error = error {
-                        print("a3....Error executing JavaScript: \(error)")
-                    } else {
-                        print("a3....Click simulated successfully.")
-                    }
-                }
+        clickIndicator.center = CGPoint(x: x, y: y)
         
-//        let js = "document.querySelector('YOUR_SELECTOR').click();"
-//        webView.evaluateJavaScript(js) { (result, error) in
-//            if let error = error {
-//                print("a3....Error executing JavaScript: \(error)")
-//            } else {
-//                print("a3...Click simulated successfully.")
-//            }
-//        }
+        let js = "var evt = new MouseEvent('click', {clientX: \(x), clientY: \(y), bubbles: true}); document.elementFromPoint(\(x), \(y)).dispatchEvent(evt);"
+        
+        webView1.evaluateJavaScript(js) { (result, error) in
+            if let error = error {
+                print("a3....Error executing JavaScript: \(error)")
+            } else {
+                print("a3....Click simulated successfully.")
+            }
+        }
     }
     
     deinit {
         // Invalidate timer when the view controller is deallocated
         timer?.invalidate()
     }
+    
+    func stopAutoClicking() {
+        selectBT.backgroundColor = UIColor.red
+           timer?.invalidate()
+           timer = nil // Clear the timer
+       }
 }
 
